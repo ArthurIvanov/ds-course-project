fd7fee9a583dbaffacb732b14acbd59bd51f544d
Arthur Ivanov <artur.dsgn@yandex.ru>
2024-02-03 22:34:28 +0300

    feat: added example-card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 import { GlobalStyle, lightTheme } from "./lib/theme";
-import { Placeholder, Button } from "./lib";
-import { ThemeProvider } from "styled-components";
+import { ExampleCard } from "./lib";
+import styled, { ThemeProvider } from "styled-components";
+import ManImage from "./lib/example-card/assets/image-3.png";
+
+const title = "Заголовок";
+const subTitle = "Подзаголовок карточки";
+const bodyText =
+	"Повседневная практика показывает, что начало повседневной работы по формированию позиции прекрасно подходит для реализации как самодостаточных, так и внешне зависимых концептуальных решений.";
+
+const StyledContainer = styled.div`
+	max-width: 800px;
+	margin: 0 auto;
+	padding-top: 16px;
+	display: flex;
+	flex-direction: row;
+	gap: 16px;
+	justify-content: stretch;
+	height: 600px;
+`;
 
 function App() {
 	const switcher = false;
@@ -9,9 +26,19 @@ function App() {
 		<ThemeProvider theme={lightTheme}>
 			<GlobalStyle />
 			<main>
-				{/* <Placeholder text="Привет мир!" /> */}
-				<Button />
-				<Button size="small" disabled />
+				<StyledContainer>
+					<ExampleCard
+						imagePath={ManImage}
+						title={title}
+						bodyText={bodyText}
+						subTitle={subTitle}
+					/>
+					<ExampleCard
+						imagePath={ManImage}
+						title="Заголовок введенный руками"
+						bodyText={bodyText}
+					/>
+				</StyledContainer>
 			</main>
 		</ThemeProvider>
 	);
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
 export * from "./placeholder";
 export * from "./button";
+export * from "./example-card";
 export * from "./theme";
